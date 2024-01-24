8a5cad23c1fd4e526bfcde283aa4ef5e5315ab21
alsumairi1990 <samnaddeen200@gmail.com>
2024-01-25 00:53:13 +0800

    service full hover added

--- app/_components/ServicesFull.tsx
+++ app/_components/ServicesFull.tsx
@@ -28,115 +28,115 @@ const ServicesFull = () => {
         <div className="grid grid-cols-2 sm:grid-cols-6 gap-x-4 gap-y-8 mt-8">
 
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+        <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_1}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Web Deveopment</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">Web Deveopment</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_2}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">banner ads design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">banner ads design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_3}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Logo design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">Logo design</h2>             
             </div>
-          </div>  
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          </a>  
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_4}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">flyer design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">flyer design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_5}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Brochure design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">Brochure design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_6}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Mobile design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">Mobile design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_7}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">label design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold  ">label design</h2>             
             </div>
-          </div>
+          </a>
 
          
 
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_8}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">T-Shirt Design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">T-Shirt Design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_9}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">bag and toat design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">bag and toat design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_10}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">flyer design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">flyer design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_11}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Resume design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold ">Resume design</h2>             
             </div>
-          </div>
+          </a>
 
-          <div className="p-2 flex flex-col justify-center py-4 sm:pt-2 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
+          <a href='' className="p-2 flex flex-col justify-center py-4 sm:pt-2 hover:bg-orange-500 hover:text-white text-orange-500 dark:text-[#ca82ef] border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#8849a9]">
             <div className="dark:pb-2 pb-3 pt-2 dark:pt-0 dark:sm:pb-2 w-16  dark:bg-[#080808]  sm:w-[9rem] dark:sm:w-[7.5rem] rounded-xl  px-4 dark:px-0 flex items-center mx-auto ">
               <img className='w-full mx-auto' src={`${imagePath_12}`} alt="" />
             </div>
             <div className="sm:p2 mt-1 text-center">
-              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold text-orange-500 dark:text-[#ca82ef]">Magazine design</h2>             
+              <h2 className="text-base sm:text-base capitalize sm:capitalize font-semibold  ">Magazine design</h2>             
             </div>
-          </div>
+          </a>
           
         </div>
         <div className="flex justify-center mt-8">
