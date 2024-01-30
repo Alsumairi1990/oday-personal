2559e1b202bf25690f170567b42e0968272aef85
alsumairi1990 <samnaddeen200@gmail.com>
2024-01-30 21:41:38 +0800

    review page completed

--- app/(pages-default)/reviews/page.tsx
+++ app/(pages-default)/reviews/page.tsx
@@ -8,11 +8,41 @@ const Reviews = () => {
    const imagePath2 = '/images/service2.png';
    const reviews = [
       {
-         name : 'Health Tracker Application ',
-         job : "Busness admin",
+         name : 'Samee Sufian ',
+         job : "Busneess Manager",
          body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
          image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1704443071_itdp-logo.jpg'
       },
+       {
+         name : 'Ali Hameed ',
+         job : "Project Analyzer",
+         body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
+         image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1684836367_mijo-logo.png'
+      },
+      {
+         name : 'Saleem Alnajeed Mohammed',
+         job : "Project Manager",
+         body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
+         image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1526467595_ask-in-world_app_icon.png'
+      },
+      {
+         name : 'Nadeer Jameel ',
+         job : "Busneess Manager",
+         body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
+         image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1652170491_telemed-logo.png'
+      },
+       {
+         name : 'Ali Hameed ',
+         job : "Project Analyzer",
+         body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
+         image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1481721237_fog_app_icon.png'
+      },
+      {
+         name : 'Alnajeed fadel',
+         job : "Project Manager",
+         body  : 'Konstant Infosolutions team was timely and committed throughout the engagement. The team led a punctual, communicative, and responsive to ensure a solid project management',
+         image : 'https://d3q8mqotzsvo6s.cloudfront.net/k_portfolio/logo/1532614408_clipkard.jpg'
+      }
       
     ]
    
@@ -22,7 +52,7 @@ const Reviews = () => {
         {/* <div className="w-full h-full absolute left-0 top-0 bg-black z-10 opacity-70"></div> */}
         <div className="flex flex-wrap w-11/12 mx-auto z-20">
            
-           <div className="flex w-full flex-col items-center justify-center">
+           <div className="flex w-full flex-col items-center justify-center ">
                 <img className="w-14 mb-2" src="https://cdn-kpbff.nitrocdn.com/BIqbPXrNizxHMrZEuWjiIDigcWJtuMrr/assets/images/optimized/rev-3d7ee36/d3mds3ychln71.cloudfront.net/img/white-heart-img.png" alt="" />
                <h2 className="text-4xl text-white tracking-wider font-bold mb-2">The Highest Standards. The Happiest Clients.</h2>
                <p className="text-md  text-gray-100 uppercase tracking-wide leading-7">WE LOVE OUR CLIENTS AND THEY LOVE US BACK. HEAR WHAT THEY’RE SAYING…</p>
@@ -68,26 +98,67 @@ const Reviews = () => {
 
         <div className="w-10/12 mx-auto mt-4 mb-10 " >
            <div className="p-4 text-center mb-4" >
-              <h2 className="text-gray-800 font-bold text-2xl mb-2">If you need us, we’re really the right people to help.</h2>
-              <p className="text-gray-600 leading-7 font-medium text-sm">READ THE REAL SUCCESS STORIES IN OUR CLIENT’S WORDS.</p>
+              <h2 className="text-gray-800 font-bold tracking-wider text-3xl mb-2">If you need us, we’re really the right people to help.</h2>
+              <p className="text-gray-600 leading-7 tracking-wide  font-medium text-md">READ THE REAL SUCCESS STORIES IN OUR CLIENT’S WORDS.</p>
            </div>
-           <div className="grid sm:grid-cols-3 gap-6">
+           <div className="grid sm:grid-cols-3 gap-8">
 
                {reviews.map((review : any) =>(
-                     <div className="flex flex-col justify-center">
-                     <div className="p-2 flex justify-center">
-                        <img src="rev" alt="" />
+                     <div className="flex flex-col items-center justify-center border border-gray-200 shadow-xl rounded-md px-4 pt-5">
+                     <div className="px-2 w-24 py-2 my-2 rounded-md  flex justify-center" style={{boxShadow:'0 0 5px #ccc'}}>
+                        <img src={review.image} alt='' />
                      </div>
-                     <div className="p-2">
-                        <h2 className="text-base font-semibold text-gray-700"></h2>
-                        <span className="w-12">
-                        <svg className="w-full " viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                              <rect width="24" height="24" fill="white"/>
-                              <path d="M11.5245 4.46353C11.6741 4.00287 12.3259 4.00287 12.4755 4.46353L13.9084 8.87336C13.9753 9.07937 14.1673 9.21885 14.3839 9.21885H19.0207C19.505 9.21885 19.7064 9.83866 19.3146 10.1234L15.5633 12.8488C15.3881 12.9761 15.3148 13.2018 15.3817 13.4078L16.8145 17.8176C16.9642 18.2783 16.437 18.6613 16.0451 18.3766L12.2939 15.6512C12.1186 15.5239 11.8814 15.5239 11.7061 15.6512L7.95488 18.3766C7.56303 18.6613 7.03578 18.2783 7.18546 17.8176L8.6183 13.4078C8.68524 13.2018 8.61191 12.9761 8.43667 12.8488L4.68544 10.1234C4.29358 9.83866 4.49497 9.21885 4.97933 9.21885H9.6161C9.83272 9.21885 10.0247 9.07937 10.0916 8.87336L11.5245 4.46353Z" className="stroke-[#ff711c]" stroke-linecap="round" stroke-linejoin="round"/>
-                           </svg>
+                     <div className="p-2 flex flex-col items-center mt-1">
+                        <h2 className="text-2xl font-bold text-center text-gray-700">{review.name}</h2>
+                        <h2 className="text-sm font-semibold uppercase text-gray-500">{review.job}</h2>
+                        <div className="flex justify-center pt-2 items-center">
+                           <span className="w-5 h-5">
+                              <svg className="w-full h-full fill-[#ff711c]" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z"></path>
+                              </svg>
+                           </span>
+                           <span className="w-5 h-5">
+                              <svg className="w-full h-full fill-[#ff711c]" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z"></path>
+                              </svg>
+                           </span>
+                           <span className="w-5 h-5">
+                              <svg className="w-full h-full fill-[#ff711c]" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z"></path>
+                              </svg>
+                           </span>
+                           <span className="w-5 h-5">
+                              <svg className="w-full h-full fill-[#ff711c]" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z"></path>
+                              </svg>
+                           </span>
+                           <span className="w-5 h-5">
+                              <svg className="w-full h-full fill-[#ff711c]" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                                 <path d="M3.488 13.184l6.272 6.112-1.472 8.608 7.712-4.064 7.712 4.064-1.472-8.608 6.272-6.112-8.64-1.248-3.872-7.808-3.872 7.808z"></path>
+                              </svg>
+                           </span>
+                          
+
+
+                        </div>
+                        
+                     </div>
+                     <div className=" flex h-36 overflow-y-auto justify-center">
+                        <p className="text-center leading-7">{review.body}</p>
+                     </div>
+                     <div className="p-2 my-5  flex items-center border border-gray-200 rounded-lg">
+                        <a href="" className="text-md text-orange-500 font-medium uppercase">Read full review</a>
+                        <span className="ml-2">
+                           <svg height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
+                                 viewBox="0 0 512 512" >
+                              <path fill="#FFFFFF" d="M256,504C119.248,504,8,392.752,8,256S119.248,8,256,8s248,111.248,248,248S392.752,504,256,504z"/>
+                              <path fill="#8AD5DD" d="M256,16c132.336,0,240,107.664,240,240S388.336,496,256,496S16,388.336,16,256S123.664,16,256,16
+                                 M256,0C114.608,0,0,114.608,0,256c0,141.376,114.608,256,256,256s256-114.624,256-256C512,114.608,397.392,0,256,0L256,0z"/>
+                              <polygon fill="#2D2D2D" points="214.656,95.936 378.016,256 214.656,416.064 165.856,366.096 278.208,256 165.856,145.904 
+                                 "/>
+                              </svg>
                         </span>
                      </div>
-                     <div className=""></div>
                   </div>
                ))}
             
