ef05e0a38bef05b74fd435403f5ae652ec3a17cf
alsumairi1990 <samnaddeen200@gmail.com>
2024-02-02 17:21:54 +0800

    service page started '

--- app/(pages-default)/services/page.tsx
+++ app/(pages-default)/services/page.tsx
@@ -756,7 +756,7 @@ const Serivices = () => {
                <h2 className="sm:text-3xl text-gray-900 capitalize font-bold tracking-wide dark:text-orange-400">How it works</h2>
                <p className="text-sm leading-7 text-center mt-1.5 text-gray-700 dark:text-gray-200">At Mobulous, we represent a well-established set of social, educational, and professional values which represent our highest ambitions for how we engage as Co-workers, Collaborators, Alumni, Associates, and Board members.</p>
             </div>
-            <div className="flex gap-6">
+            <div className="flex flex-wrap gap-6 max-sm:p-4">
             {phases.map((phase, index:number) => (
                <ProcessPhase key={phase.id} phase={phase} index={index} />
             ))}
--- app/_components/_services/ProcessPhase.tsx
+++ app/_components/_services/ProcessPhase.tsx
@@ -9,13 +9,17 @@ interface PhaseIntProps {
     index: number;
   }
 const ProcessPhase = ({ phase,index } : PhaseIntProps ) => {
+  const phaseKeys = Object.keys(phase);
+  const isLastIndex = index === phaseKeys.length - 1;
+  
   return (
    
-        <div className="rounded-md flex flex-col shadow-md items-center relative flex-2 border border-gray-300 dark:border-gray-600 dark:bg-black-150">
-          <span className="flex absolute -right-5 "><RiArrowRightFill /></span>
-            <div className="p-1.5  w-14 h-14 bg-white flex flex-col items-center absolute -top-4 rounded-full border border-gray-400">
+        <div className="rounded-md flex flex-col max-sm:mb-8 shadow-md  items-center relative sm:flex-18 border border-gray-300 dark:border-gray-600 dark:bg-black-150">
+          {!isLastIndex && (
+            <span className="flex absolute -bottom-6 sm:top-4 sm:-right-6 "><RiArrowRightFill className='text-xl max-sm:rotate-90 text-blue-500' /></span>
+          )}
+            <div className="p-1.5  w-14 h-14 bg-gray-50 flex flex-col items-center shadow-md absolute -top-6 rounded-full border border-gray-400">
               <img className='  rounded-full' src={phase.image} alt="" />
-
            </div>
          
           <div className="flex mt-12 mb-2 items-center justify-center">
@@ -26,22 +30,25 @@ const ProcessPhase = ({ phase,index } : PhaseIntProps ) => {
             <p className="text-sm leading-6 text-gray-700 dark:text-gray-300">{phase.desc}</p>
           </div>
           <div className="mt-3 p-2">
-          {phase.steps && phase.steps.length > 0 && (
-          <div className='mt-2'>
-            <ul className='flex flex-col px-4'>
-              {phase.steps.map((step) => (
-                <li className='flex items-center py-2.5 border-b border-gray-300 dark:border-b-gray-700'  key={step.id}>
-                  <span className="flex h-2 w-2 rounded-full bg-red-400 dark:bg-violet-500 mr-2.5"></span>
-                  <h3 className='text-sm text-gray-700 dark:text-gray-300 font-semibold'>{step.name}</h3>
-                </li>
-              ))}
-            </ul>
-          
+                  {
+                    phase.steps && phase.steps.map((step, index) => (
+                      <div className='flex items-center' key={step.id}>
+                        
+                        {phase.steps && index === phase.steps.length - 1 ? (
+                          <li className='flex w-full items-center py-2.5' key={step.id}>
+                          <span className="flex h-2 w-2 rounded-full bg-red-400 dark:bg-violet-500 mr-2.5"></span>
+                          <h3 className='text-sm text-gray-700 dark:text-gray-300 font-semibold'>{step.name}</h3>
+                          </li>
+                          ) : (
+                              <li className='flex w-full items-center py-2.5 border-b border-gray-200 dark:border-b-gray-700' key={step.id}>
+                                <span className="flex h-2 w-2 rounded-full bg-red-400 dark:bg-violet-500 mr-2.5"></span>
+                                <h3 className='text-sm text-gray-700 dark:text-gray-300 font-semibold'>{step.name}</h3>
+                            </li>
+                        )}
+                      </div>
+                    ))
+                  }
           </div>
-        )}
-            
-          </div>
-           
     </div>
   );
 };
