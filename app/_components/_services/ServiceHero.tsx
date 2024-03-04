1d6d6473739b106f45d1f8851ddeb42c196c6261
alsumairi1990 <samnaddeen200@gmail.com>
2024-03-04 08:04:52 +0800

    nav bar updated

--- app/_components/_services/ServiceHero.tsx
+++ app/_components/_services/ServiceHero.tsx
@@ -9,7 +9,9 @@ const ServiceHero = ({ service }: ServiceProps) => {
   return (
         <div className="w-full " >  
            <div className=" m-h-lvh sm:min-h-[28rem] sm:h-[28rem] pb-4 sm:pb-0 pt-[100px] flex px-4 relative w-full bg-no-repeat bg-center bg-cover -z-0" style={{backgroundImage: `url(${service.image})`}}>
-            <div className="w-full h-full absolute left-0 top-0 bg-black z-10 opacity-60"></div>
+            {/* <div className="w-full h-full absolute left-0 top-0 bg-black z-10 opacity-60"></div> */}
+            <div className="absolute top-0 h-full w-full left-0" style={{backgroundImage: 'linear-gradient(to top, rgb(8 8 8),rgb(0 0 0 / 55%), rgb(8 8 8))'}}></div>
+
             <div className="flex flex-wrap w-11/12 mx-auto z-20">
             <div className="flex w-full flex-col items-center justify-center ">
                 <h2 className="text-4xl text-white tracking-wider font-bold mb-1">The Highest Standards. The Happiest Clients.</h2>
