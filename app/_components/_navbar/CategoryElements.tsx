cae2d9bc5c45fa30e600dc46d921317e3b493f6e
alsumairi1990 <samnaddeen200@gmail.com>
2024-03-07 10:01:42 +0800

    working on mobile nav

--- app/_components/_navbar/CategoryElements.tsx
+++ app/_components/_navbar/CategoryElements.tsx
@@ -71,12 +71,12 @@ interface ServiceCategoryProps {
             <ul className='grid grid-cols-2 sm:flex max-sm:mt-6 sm:flex-col max-sm:gap-4 sm:px-4 max-sm:px-6'>
               {serviceCategory.services.map((service) => (
                 
-                <li className='flex items-center py-2 max-sm:px-2 sm:py-1 max-sm:border max-sm:border-gray-300 max-sm:rounded-md'  key={service.id}>
-                  <div className="max-sm:flex-15 bg-white border w-[22px] border-gray-400 p-1 mr-2  justify-center flex items-center rounded-full "  >
+                <li className='flex max-sm:flex-col items-center py-2 max-sm:shadow-md max-sm:px-2 sm:py-1 max-sm:border max-sm:border-gray-300 max-sm:rounded-md'  key={service.id}>
+                  <div className=" bg-white border max-sm:mb-2 w-[26px] sm:w-[22px] border-gray-400 p-1 mr-2  justify-center flex items-center rounded-full "  >
                     <img className='w-full' src={service.image} alt={service.name} />
                   </div>
                   
-                  <h3 className='max-sm:flex-85 text-sm sm:text-sm text-gray-800 font-medium '>{service.name}</h3>
+                  <h3 className='text-sm sm:text-sm text-gray-800 font-medium max-sm:text-center'>{service.name}</h3>
                   
                 </li>
               ))}
