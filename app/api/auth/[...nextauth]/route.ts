372ba37b0c076e7089563291ef9eed1695fedf36
alsumairi1990 <samnaddeen200@gmail.com>
2024-05-28 10:59:05 +0800

    nextauth upadate-4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,68 +1,35 @@
-import { AuthOptions } from "next-auth";
 
-
-
-import prisma from "@/utils/prisma";
-import CredentialsProvider  from "next-auth/providers/credentials";
-import * as bcrypt from "bcrypt";
 import NextAuth from "next-auth/next";
-import { User } from "@prisma/client";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export const  authOptions : AuthOptions= {
-  // pages: {
-  //   signIn: "/auth/signin",
-  // },
-    providers: [
-        CredentialsProvider({
-            name: "Credentials",
-      
-            credentials: {
-              username: {
-                label: "User Name",
-                type: "text",
-                placeholder: "Your User Name",
-              },
-              password: {
-                label: "Password",
-                type: "password",
-              },
-            },
-            async authorize(credentials) {
-              const user = await prisma.user.findUnique({
-                where: {
-                  email: credentials?.username,
-                },
-              });
-      
-              if (!user) throw new Error("User name or password is not correct");
-      
-              // This is Naive Way of Comparing The Passwords
-              // const isPassowrdCorrect = credentials?.password === user.password;
-              if (!credentials?.password) throw new Error("Please Provide Your Password");
-              const isPassowrdCorrect = await bcrypt.compare(credentials.password, user.password);
-      
-              if (!isPassowrdCorrect) throw new Error("User name or password is not correct");
-      
-              if (!user.email) throw new Error("Please verify your email first!");
-      
-              const { password, ...userWithoutPass } = user;
-              return userWithoutPass;
-            },
-          }),
-    ],
-    callbacks: {
-      async jwt({ token, user }) {
-        if (user) token.user = user as User;
-        return token;
-      },
-  
-      async session({ token, session }) {
-        session.user = token.user;
-        return session;
-      },
-    },
-};
-const handler = NextAuth(authOptions);
+import { compare } from "bcrypt";
 
-// export { handler as GET, handler as POST };
-export default handler;
+const handler = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+
+  pages: {
+    signIn: "/login",
+  },
+
+  providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        email: {},
+        password: {},
+      },
+      async authorize(credentials, req) {
+        return null;
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
