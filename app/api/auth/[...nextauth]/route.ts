55548df29faf393c146762c78a99a5051523a2c6
alsumairi1990 <samnaddeen200@gmail.com>
2024-05-28 10:29:15 +0800

    nextauth upadate-3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -9,9 +9,9 @@ import NextAuth from "next-auth/next";
 import { User } from "@prisma/client";
 
 export const  authOptions : AuthOptions= {
-  pages: {
-    signIn: "/auth/signin",
-  },
+  // pages: {
+  //   signIn: "/auth/signin",
+  // },
     providers: [
         CredentialsProvider({
             name: "Credentials",
@@ -63,5 +63,6 @@ export const  authOptions : AuthOptions= {
     },
 };
 const handler = NextAuth(authOptions);
-export default NextAuth(authOptions);
+
 // export { handler as GET, handler as POST };
+export default handler;
