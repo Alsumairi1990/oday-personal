563c2f193021f89d045aafa3a3084bc8aa892e5b
alsumairi1990 <samnaddeen200@gmail.com>
2024-05-29 15:44:57 +0800

    nextauth upadate-26

--- middleware.ts
+++ middleware.ts
@@ -2,5 +2,6 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   // specify the route you want to protect
-  matcher: ["/:path*"],
+  matcher: ["/((?!.*\\.(?:png|jpg|jpeg|svg|gif|ico|xml|json|txt|css|js)).*)"],
+
 };
