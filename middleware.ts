96062c9157754bf0b4565c942fe2e697955e869e
alsumairi1990 <samnaddeen200@gmail.com>
2024-05-29 16:31:35 +0800

    nextauth upadate-26

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,5 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   // specify the route you want to protect
-  matcher: [
-    "^/admin(?=/.*)?$", // Match '/admin' and any nested paths
-  ],
+  matcher: ['/admin/:path*'],
 };
