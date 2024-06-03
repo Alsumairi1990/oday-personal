ed2e5ee17daf44fce2952a7d81e98014fc304340
alsumairi1990 <samnaddeen200@gmail.com>
2024-06-04 03:14:50 +0800

    service create

--- app/_components/admin/service/ServiceCreate.tsx
+++ app/_components/admin/service/ServiceCreate.tsx
@@ -80,12 +80,12 @@ const ServiceCreate = () => {
              <div className="add-main">
                 <AddingBtn
                 title="Tech Details"
-                svgUrl={basic}
+                svgUrl={extra}
                 title_name="addation"
                 changeMenu={changeBtnMenu}
                 />
                 {isOpen('addation') && (<div className="fixed flex add-form bg-[#00000061]  items-center justify-center top-0 left-0 h-full w-full z-50">
-                        <div   className="flex flex-col w-full sm:w-6/12 add-menu  bg-white items-center rounded-md  border border-gray-300" style={{boxShadow: 'rgb(82 63 104 / 12%) 0px 0px 10px 0px'}}>
+                        <div   className="flex flex-col w-full max-sm:h-full sm:w-6/12 add-menu  bg-white items-center rounded-md  border border-gray-300" style={{boxShadow: 'rgb(82 63 104 / 12%) 0px 0px 10px 0px'}}>
                           <div className="flex w-full bg-[#7c3aed] rounded-t-md py-2.5 items-center px-3 border-b border-b-gray-300">
                               <div className="flex items-center">
                                   <span className="inline-flex h-full w-9 pb-1 justify-center items-center">
