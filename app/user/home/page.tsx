5ccc85242ccca2ba39980001233a8c96dec59c67
alsumairi1990 <samnaddeen200@gmail.com>
2024-06-09 00:00:38 +0800

    user dasboard  added

--- app/user/home/page.tsx
+++ app/user/home/page.tsx
@@ -1,1 +1,14 @@
-p
+import Service from "@/app/_components/admin/service/ManageService";
+
+const HomePage = () => {
+
+    const imagePath1 = '/images/navbg.webp';
+
+    return (
+      <div className="p-2">
+        <p className="text-gray-600 pt-3 mb-8">User Page</p>
+        <Service  />
+      </div>
+)
+};
+export default HomePage;
