fe33b452547267e39b18c0b3a66ff365938b344c
Ahmed Alsumairi <47456452+AhmedAlsumairi@users.noreply.github.com>
2024-06-17 18:36:12 +0000

    fotn color added to editor

--- app/_components/admin/editor/TextEditor.tsx
+++ app/_components/admin/editor/TextEditor.tsx
@@ -500,12 +500,78 @@ const TextEditor = () => {
                     className={`flex-15 bg-[#377a24] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#377a24" }) ? "is-active" : ""}`}
                     data-testid="setGreen"
                   ></button>
-                  {/*   <button
-                                                        onClick={() => editor.chain().focus().setColor('#F98181').run()}
-                                                        className={`flex-15 bg-[#F98181] text-sm w-[25px] font-semibold text-gray-600 ${editor.isActive('textStyle', { color: '#958DF1' }) ? 'is-active' : ''}`}
-                                                        data-testid="setRed"
-                                                      >  
-                                                  </button>  */}
+
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#300a24").run()
+                    }
+                    className={`flex-15 bg-[#300a24] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#300a24" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#b7b700").run()
+                    }
+                    className={`flex-15 bg-[#b7b700] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#b7b700" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#007497").run()
+                    }
+                    className={`flex-15 bg-[#007497] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#007497" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#0c56df").run()
+                    }
+                    className={`flex-15 bg-[#0c56df] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#0c56df" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#980cdf").run()
+                    }
+                    className={`flex-15 bg-[#980cdf] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#980cdf" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#0c4ddf").run()
+                    }
+                    className={`flex-15 bg-[#0c4ddf] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#0c4ddf" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#b10039").run()
+                    }
+                    className={`flex-15 bg-[#b10039] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#b10039" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#00a982").run()
+                    }
+                    className={`flex-15 bg-[#00a982] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#00a982" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#2e2e2e").run()
+                    }
+                    className={`flex-15 bg-[#2e2e2e] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#2e2e2e" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
+                  <button
+                    onClick={() =>
+                      editor.chain().focus().setColor("#0aa6bf").run()
+                    }
+                    className={`flex-15 bg-[#0aa6bf] text-sm w-[25px] h-6 font-semibold text-gray-600 ${editor.isActive("textStyle", { color: "#0aa6bf" }) ? "is-active" : ""}`}
+                    data-testid="setGreen"
+                  ></button>
                 </div>
                 <div className="flex items-center border w-auto rounded bg-gray-100 text-md font-semibold px-1 self-center my-2">
                   <span className="text-gray-500 mr-2">More Colors</span>
