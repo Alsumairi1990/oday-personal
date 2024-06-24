189f49631b6b8cc7c7bbc6b3f7cf49e1355c593a
Ahmed Alsumairi <47456452+AhmedAlsumairi@users.noreply.github.com>
2024-06-24 04:23:05 +0000

    aligining text  added

--- app/_components/admin/editor/TextEditor.tsx
+++ app/_components/admin/editor/TextEditor.tsx
@@ -1,7 +1,8 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import Document from "@tiptap/extension-document";
+import HorizontalRule from "@tiptap/extension-horizontal-rule";
 import { Color } from "@tiptap/extension-color";
 import Placeholder from "@tiptap/extension-placeholder";
 import Heading from "@tiptap/extension-heading";
@@ -12,12 +13,20 @@ import Text from "@tiptap/extension-text";
 import TextStyle from "@tiptap/extension-text-style";
 import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
+import TextAlign from "@tiptap/extension-text-align";
+import Superscript from "@tiptap/extension-superscript";
+import Subscript from "@tiptap/extension-subscript";
+import Blockquote from "@tiptap/extension-blockquote";
+import HardBreak from "@tiptap/extension-hard-break";
 
 import { FaBold } from "react-icons/fa6";
 import { FaItalic } from "react-icons/fa";
 import { FaStrikethrough } from "react-icons/fa6";
 import { FaUnderline } from "react-icons/fa6";
 import { IoMdColorFill } from "react-icons/io";
+import Link from "@tiptap/extension-link";
+
+// import "../../../globals.css";
 
 import { FaCaretDown } from "react-icons/fa";
 
@@ -87,14 +96,28 @@ const TextEditor = () => {
   const editor = useEditor({
     extensions: [
       StarterKit,
+      Document,
       Bold,
+      TextAlign.configure({
+        types: ["heading", "paragraph"],
+      }),
+      Subscript,
       Text,
+      Blockquote,
+      HorizontalRule,
       Heading.configure({
         levels: [1, 2, 3],
       }),
       TextStyle,
       Underline,
+      Superscript,
+      HardBreak,
       Color,
+      Link.configure({
+        openOnClick: false,
+        autolink: true,
+        defaultProtocol: "https",
+      }),
       Highlight.configure({ multicolor: true }),
       Placeholder.configure({
         emptyEditorClass: "is-editor-empty",
@@ -111,6 +134,33 @@ const TextEditor = () => {
       },
     },
   });
+
+  const setLink = useCallback(() => {
+    const previousUrl = editor!.getAttributes("link").href;
+    const url = window.prompt("URL", previousUrl);
+
+    // cancelled
+    if (url === null) {
+      return;
+    }
+
+    // empty
+    if (url === "" && editor != null) {
+      editor.chain().focus().extendMarkRange("link").unsetLink().run();
+
+      return;
+    }
+
+    // update link
+    if (editor) {
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange("link")
+        .setLink({ href: url })
+        .run();
+    }
+  }, [editor]);
   if (!editor) {
     return null;
   }
@@ -823,18 +873,280 @@ const TextEditor = () => {
               )}
             </div>
           </div>
-        </div>
 
-        {/* <div className="flex border-b bg-[#dbdbdb14] border-b-gray-200 bg-gradioent-to-t from-[#e1e4e5] to-[#fbfbfb]"  >
-           <div className="relative edit-btn flex flex-col items-center ">
-                   <button
-                    onClick={() => editor.chain().focus().toggleBold().run()}
-                    className={editor.isActive('bold') ? 'is-active' : ''}
-                     >
-                    toggleBold
-                  </button>
-       </div>
-       </div>*/}
+          {/* --------------------------------------------------------------------------------------------- */}
+          {/* Start Align Area */}
+          <div className="py-2 px-1 flex flex-10 gap-y-3 flex-wrap items-center border-r border-r-gray-200">
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().setTextAlign("left").run()}
+              // className="flex-25 p-[2px] flex justify-center"
+              className={
+                editor.isActive({ textAlign: "left" }) ? "is-active" : ""
+              }
+            >
+              <svg
+                width="28px"
+                height="28px"
+                viewBox="-2 -2 20 20"
+                fill="#111"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  margin: "0px",
+                  border: "0px",
+                  alignSelf: "self-start",
+                }}
+              >
+                <path
+                  d="M2 12.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              // className="flex-25 p-[2px] flex justify-center"
+              onClick={() =>
+                editor.chain().focus().setTextAlign("center").run()
+              }
+              className={
+                editor.isActive({ textAlign: "center" }) ? "is-active" : ""
+              }
+            >
+              <svg
+                width="28px"
+                height="28px"
+                viewBox="-2 -2 20 20"
+                fill="#111"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  margin: "0px",
+                  border: "0px",
+                  alignSelf: "self-start",
+                }}
+              >
+                <path
+                  d="M4 12.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm2-3a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-2-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              // className="flex-25 p-[2px] flex justify-center"
+              onClick={() => editor.chain().focus().setTextAlign("right").run()}
+              className={
+                editor.isActive({ textAlign: "right" }) ? "is-active" : ""
+              }
+            >
+              <svg
+                width="28px"
+                height="28px"
+                viewBox="-2 -2 20 20"
+                fill="#111"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  margin: "0px",
+                  border: "0px",
+                  alignSelf: "self-start",
+                }}
+              >
+                <path
+                  d="M6 12.5a.5.5 0 01.5-.5h7a.5.5 0 010 1h-7a.5.5 0 01-.5-.5zm-4-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              // className="flex-25 p-[2px] flex justify-center"
+              onClick={() => editor.chain().focus().toggleSubscript().run()}
+              className={editor.isActive("subscript") ? "is-active" : ""}
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="#222"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 18H5L13 4H15M3 4H5L13 18H15M17 15L19 14V20M19 20H17M19 20H21"
+                  stroke="#555"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              // className="flex-25 p-[2px] flex justify-center is-active"
+              onClick={() =>
+                editor.chain().focus().setTextAlign("justify").run()
+              }
+              className={
+                editor.isActive({ textAlign: "justify" }) ? "is-active" : ""
+              }
+            >
+              <svg
+                width="28px"
+                height="28px"
+                viewBox="-2 -2 20 20"
+                fill="#111"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  margin: "0px",
+                  border: "0px",
+                  alignSelf: "self-start",
+                }}
+              >
+                <path
+                  d="M2 12.5a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5zm0-3a.5.5 0 01.5-.5h11a.5.5 0 010 1h-11a.5.5 0 01-.5-.5z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              // className="flex-25 p-[2px] flex justify-center"
+              onClick={() => editor.chain().focus().toggleSuperscript().run()}
+              className={editor.isActive("superscript") ? "is-active" : ""}
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M9 13L5 6H3M9 13L13 20H15M9 13L13 6H15M9 13L5 20H3M17 4L19 3V9M19 9H17M19 9H21"
+                  stroke="#444"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              className="flex-25 p-[2px] flex justify-center"
+              onClick={() => editor.chain().focus().unsetTextAlign().run()}
+            >
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+                id="align-left-slash"
+                data-name="Flat Line"
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon flat-line"
+              >
+                <path
+                  id="primary"
+                  d="M21,6H18M12,6H3m14,6H12M6,12H3m18,6H6M21,3,3,21"
+                  style={{
+                    fill: "none",
+                    stroke: "rgb(85, 85, 85)",
+                    strokeLinecap: "round",
+                    strokeLinejoin: "round",
+                    strokeWidth: "2",
+                  }}
+                ></path>
+              </svg>
+            </button>
+          </div>
+          {/* End Align Area */}
+          {/* --------------------------------------------------------------------------------------------- */}
+
+          {/* Start third - 3   Area */}
+          <div className="py-2 px-1 flex flex-15 gap-y-3 flex-wrap items-center border-r border-r-gray-200">
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().toggleBlockquote().run()}
+              className={`flex-20 px-1 ${editor.isActive("blockquote") ? "is-active" : ""}`}
+            >
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="-3 -3 40 40"
+                fill="#5F6368"
+                className=" m-0 border-0"
+              >
+                <path d="M12,15H6.11A9,9,0,0,1,10,8.86l1.79-1.2L10.69,6,8.9,7.2A11,11,0,0,0,4,16.35V23a2,2,0,0,0,2,2h6a2,2,0,0,0,2-2V17A2,2,0,0,0,12,15Z"></path>
+                <path d="M26,15H20.11A9,9,0,0,1,24,8.86l1.79-1.2L24.7,6,22.9,7.2A11,11,0,0,0,18,16.35V23a2,2,0,0,0,2,2h6a2,2,0,0,0,2-2V17A2,2,0,0,0,26,15Z"></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().setHorizontalRule().run()}
+              className="flex-20 px-1"
+            >
+              <svg
+                width="18px"
+                height="18px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 12L20 12"
+                  stroke="#444"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                ></path>
+              </svg>
+            </button>
+            <button
+              type="button"
+              onClick={() => editor.chain().focus().setHardBreak().run()}
+              className="flex-20 px-1"
+            >
+              <svg
+                className="icon text-gray-200"
+                width="18px"
+                height="18px"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#444"
+                viewBox="0 0 24 24"
+              >
+                <g>
+                  <path fill="none" d="M0 0h24v24H0z"></path>
+                  <path d="M15 18h1.5a2.5 2.5 0 1 0 0-5H3v-2h13.5a4.5 4.5 0 1 1 0 9H15v2l-4-3 4-3v2zM3 4h18v2H3V4zm6 14v2H3v-2h6z"></path>
+                </g>
+              </svg>
+            </button>
+
+            <button
+              type="button"
+              onClick={setLink}
+              className={editor.isActive("link") ? "is-active" : ""}
+              // className="flex-20 px-1"
+            >
+              <svg
+                className="icon text-gray-200"
+                width="19"
+                height="19"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="#444"
+                viewBox="0 0 24 24"
+              >
+                <g>
+                  <path fill="none" d="M0 0h24v24H0z"></path>
+                  <path d="M18.364 15.536L16.95 14.12l1.414-1.414a5 5 0 1 0-7.071-7.071L9.879 7.05 8.464 5.636 9.88 4.222a7 7 0 0 1 9.9 9.9l-1.415 1.414zm-2.828 2.828l-1.415 1.414a7 7 0 0 1-9.9-9.9l1.415-1.414L7.05 9.88l-1.414 1.414a5 5 0 1 0 7.071 7.071l1.414-1.414 1.415 1.414zm-.708-10.607l1.415 1.415-7.071 7.07-1.415-1.414 7.071-7.07z"></path>
+                </g>
+              </svg>
+            </button>
+          </div>
+          {/* End of  third - 3   Area */}
+          {/* --------------------------------------------------------------------------------------------- */}
+        </div>
       </div>
       <EditorContent editor={editor} />
     </div>
