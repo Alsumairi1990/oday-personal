f5d71bc72e7f029d144e52128705ab44889ad97a
Alsumairi1990 <samnaddeen2000@gmail.com>
2024-09-15 12:37:24 -0400

    before i18n added

---
+++ app/(pages)/layout.tsx
@@ -0,0 +1,20 @@
+import Footerk from "../_components/Footer.jsx"
+// import Nav from "./Nav"
+
+
+export default function signupLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+            
+
+            <div className="h-full">
+            {children}
+           
+            </div>
+          
+    )
+  }
+     
