126c0af56b1a4389d4fe76cdd2319b7a2cd73594
Alsumairi1990 <samnaddeen2000@gmail.com>
2024-10-10 05:04:15 -0400

    editting category section in main page

--- app/_components/Services.tsx
+++ app/_components/Services.tsx
@@ -17,7 +17,7 @@ const Services = async ({categories,meta}:Props) => {
   return (
 
       <div className="flex flex-col sm:py-8  dark:bg-[#080808]">
-        <div className="p-1 w-11/12 mx-auto">
+        <div className="p-1 w-11.7/12 sm:w-11/12 mx-auto">
         {locale == 'en' ? <h2 className="text-3xl text-center px-8 mb-6 text-blue-700 dark:text-white font-semibold">{meta.title}</h2>
           : <h2 className="text-3xl font-arabic rtl:font-bold rtl:mb-3 text-center px-8 mb-6 text-blue-700 dark:text-white font-normal">{meta.titleAr}</h2>
           }
@@ -26,18 +26,18 @@ const Services = async ({categories,meta}:Props) => {
           :<p className="text-base font-arabic rtl:text-gray-700 text-gray-600 dark:text-gray-300 text-center leading-7 px-8">{meta.descAr}</p>
           }
        
-        <div className="grid grid-cols sm:grid-cols-4 gap-8 mt-8">
+        <div className="grid grid-cols-2 sm:grid-cols-4 gap-x-2 gap-y-8 sm:gap-8 mt-8">
 
          {categories && categories.map((service) => (
-               <div className="p-2 flex flex-col justify-center pt-4 sm:pt-6 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#474747]">
-               <div className="dark:pb-2 pb-1 pt-1 dark:pt-0 h-20 dark:sm:pb-5 w-24 bg-gray-300 dark:bg-[#080808]  sm:w-[6.5rem] dark:sm:w-[6.5rem] rounded-xl  px-1 dark:px-0 flex items-center mx-auto ">
+               <div className="py-2 sm:p-2 flex flex-col justify-center pt-4 sm:pt-6 border shadow-lg dark:shadow-0 dark:max-sm:bg-[#171717] rounded-xl border-gray-200 dark:border-gray-600 dark:sm:border-[#474747]">
+               <div className="dark:pb-2 pb-1 pt-1 dark:pt-0 h-20 dark:sm:pb-5 overflow-hidden bg-gray-300 dark:bg-[#080808]  sm:w-[6.5rem] dark:sm:w-[6.5rem] rounded-xl  px-1 dark:px-0 flex items-center mx-auto ">
                 {service.image && <img className='w-full h-full mx-auto rounded-md' src={service.image} alt="" /> }
                </div>
                <div className="sm:p2 mt-4 text-center">
                  {/* <h2 className="text-base sm:text-xl capitalize sm:uppercase font-semibold text-orange-500 dark:text-[#ca82ef]">{service.name}</h2> */}
     
                  {locale == 'en' ? <h2 className="text-base sm:text-xl capitalize sm:uppercase font-semibold text-orange-500 dark:text-[#ca82ef]">{service.name} </h2>
-                 :  <h2 className="text-xl font-arabic rtl:font-bold rtl:mb-3 text-center px-3 mb-6 text-blue-700 dark:text-white font-normal">{service.nameAr}</h2>
+                 :  <h2 className="text-lg font-arabic rtl:font-bold rtl:mb-3 text-center px-3 mb-6 text-blue-700 dark:text-white font-normal">{service.nameAr}</h2>
                   }
                    {locale == 'en' ? <p className="text-sm leading-[22px] text-gray-500 dark:text-gray-300 text-center sm:leading-6 line-clamp-4 mt-1.5 sm:mt-3 sm:px-3">{service.description}</p>
                     :<p className="text-md font-arabic rtl:text-gray-700 text-gray-600 dark:text-gray-300 text-center leading-7 line-clamp-5 px-3">{service.descriptionAr}</p>
@@ -52,7 +52,7 @@ const Services = async ({categories,meta}:Props) => {
                    <svg height="15px" width="15px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" 
                            viewBox="0 0 512 512" >
                          <path fill="#FFFFFF" d="M256,504C119.248,504,8,392.752,8,256S119.248,8,256,8s248,111.248,248,248S392.752,504,256,504z"/>
-                         <path fill="#8AD5DD" d="M256,16c132.336,0,240,107.664,240,240S388.336,496,256,496S16,388.336,16,256S123.664,16,256,16
+                        <path fill="#8AD5DD" d="M256,16c132.336,0,240,107.664,240,240S388.336,496,256,496S16,388.336,16,256S123.664,16,256,16
                            M256,0C114.608,0,0,114.608,0,256c0,141.376,114.608,256,256,256s256-114.624,256-256C512,114.608,397.392,0,256,0L256,0z"/>
                          <polygon fill="#2D2D2D" points="214.656,95.936 378.016,256 214.656,416.064 165.856,366.096 278.208,256 165.856,145.904 
                            "/>
