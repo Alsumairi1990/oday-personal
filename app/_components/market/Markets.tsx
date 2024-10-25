b4ad311f4723598b539ba4e0c28343f041d916dc
Alsumairi1990 <samnaddeen2000@gmail.com>
2024-10-25 16:50:23 -0400

    working on blog

--- app/_components/market/Markets.tsx
+++ app/_components/market/Markets.tsx
@@ -24,7 +24,8 @@ const Markets = async ({markets,locale,messages}:Props) => {
     // const mFlag = "/market/icons/malaysia-flag.png";
     // const yFlag = "/market/icons/yemen-flag.png";
     const title = (messages as any).Common.loctaionTitle;
-    const subTitle = (messages as any).Common.locationSubTitle;
+    const subTitle = (messages as any).Common.locationSubTitle;  
+    const marketDetails = (messages as any).Common.marketDetails;
 
 
   return (
@@ -33,7 +34,7 @@ const Markets = async ({markets,locale,messages}:Props) => {
     <div className=" m-h-lvh sm:h-[30rem] justify-center pb-4 sm:pb-0 pt-8 flex  px-4 relative w-full bg-no-repeat bg-center bg-cover -z-0" style={{backgroundImage: `url(${img1})`}}>
       <div className="">
       <div className="flex flex-col gap-y-2 items-center mb-5">
-      <h2 className="text-gray-50 rtl:font-arabic text-2xl font-semibold">m {markets.length}--{title}</h2>
+      <h2 className="text-gray-50 rtl:font-arabic text-2xl font-semibold">{title}</h2>
       <p className="text-md text-gray-100 rtl:font-arabic ">{subTitle}</p>
       </div>
       <div className="grid grid-cols-1  mx-auto sm:grid-cols-3 sm:w-11/12 gap-6 pb-5">
@@ -56,68 +57,56 @@ const Markets = async ({markets,locale,messages}:Props) => {
                     className='mx-auto border-2 border-white  rounded-full p-0.5'
                     />
                 </div>
-              
-                {locale === 'en' ? 
-                <>
-                 <Link href="/markets/saudi-arabia" className="text-gray-100 sm:text-lg rtl:font-arabic border-b border-gray-300 px-2 py-1 rtl:font-bold">{market.name}</Link>
-                <div className="p- flex gap-2 justify-center flex-wrap mt-4">
-                  {market.marketPages.map((page)=> (
-                     <div className="flex rounded px-2 gap-x-1 py-1 text-sm bg-gray-100 text-gray-800 items-center">
-                     <Link href={`/markets/saudi-arabia/services/`} className=' capitalize '>{page.mrPage.name}</Link>
-                     <LuArrowRight className='text-base rtl:rotate-1 text-gray-800 ' />
-                   </div>
-                  ))}
+                <div className="text-center">
+                    {locale === 'en' ? 
+                    <>
+                    <Link href="/markets/saudi-arabia" className="text-gray-100 sm:text-lg rtl:font-arabic border-b border-gray-300 px-2 py-1 rtl:font-bold">{market.name}</Link>
+                    <div className=" flex gap-2 justify-center flex-wrap mt-4">
+                      {market.marketPages.map((page)=> (
+                        <div className="flex rounded px-2 gap-x-1 py-1  text-sm bg-gray-100 text-gray-800 items-center">
+                        <Link href={`/markets/saudi-arabia/services/`} className=' capitalize '>{page.mrPage.name}</Link>
+                        <LuArrowRight className='text-base rtl:rotate-1 text-gray-800 ' />
+                      </div>
+                      ))}
+                    </div>
+                    </>
+                    :
+                      <>
+                      <Link href="/markets/saudi-arabia" className="text-gray-100 sm:text-lg rtl:font-arabic border-b border-gray-300 px-2 py-1 rtl:font-bold">{market.nameAr}</Link>
+                    <div className="p- flex gap-2 justify-center flex-wrap mt-4">
+                    {market.marketPages.map((page)=> (
+                        <div className="flex rounded px-1 gap-x-1 py-1 text-sm bg-gray-100 text-gray-800 items-center">
+                        <LuArrowRight className='text-base rtl:rotate-1 text-gray-800 ' />
+                        <Link href={page.url!} className='font-arabic capitalize '>{page.mrPage.nameAr}</Link>
+                        
+                      </div>
+                      ))}
+                    </div>
+                    </>
+                    }
                 </div>
-                </>
-                :
-                  <>
-                  <Link href="/markets/saudi-arabia" className="text-gray-100 sm:text-lg rtl:font-arabic border-b border-gray-300 px-2 py-1 rtl:font-bold">{market.nameAr}</Link>
-                 <div className="p- flex gap-2 justify-center flex-wrap mt-4">
-                 {market.marketPages.map((page)=> (
-                     <div className="flex rounded px-1 gap-x-1 py-1 text-sm bg-gray-100 text-gray-800 items-center">
-                    <LuArrowRight className='text-base rtl:rotate-1 text-gray-800 ' />
-                     <Link href={page.url!} className='font-arabic capitalize '>{page.mrPage.nameAr}</Link>
+                <div className="mt-4 flex justify-center relative w-full" >
                      
-                   </div>
-                  ))}
-                 </div>
-                 </>
-                
-                }
+                          <Link
+                          href={`/markets/${market.location}`}
+                            className="capitalize rtl:font-arabic border z-20 
+                            relative after:content[''] after:absolute after:top-1/2 after:transform after:-translate-y-1/2 after:border-y-[15px] after:border-l-[25px] after:border-r-[0px] after:border-y-transparent after:-right-[25px]  
+                            before:content[''] before:absolute before:top-1/2 before:transform before:-translate-y-1/2 before:border-y-[15px] before:border-r-[25px] before:border-l-[0px] before:border-y-transparent before:-left-[25px] 
+                             border-orange-600 text-gray-50 text-sm px-2.5 rounded-md py-1.5 font-semibold bg-orange-600 "
+                          >
+                            {marketDetails}
+                          </Link>
+                        
+                      
+                      <span className="h-[0.1rem] w-full bg-gray-300 dark:bg-gray-800 absolute z-10 top-4">
+                      </span>
+                  </div>
+                        
                 </div>       
           </div>
 
           ))}
           
-
-          {/* <div className="p-0.5 sm:h-80 overflow-hidden h-40 relative flex items-center justify-center border border-gray-500 rounded-md">
-               <Image 
-                      src={m2}
-                      height={800}
-                      width={800}
-                      alt="Product Image"
-                      className='w-full  mx-auto rounded-t-md sm:rounded-md'
-                      />
-
-                <div className="absolute">
-                  <h2 className="text-gray-100 text-lg rtl:font-arabic border-2 border-gray-300 px-2 py-1 rounded bg-gray-800">ماليزيا</h2>
-                  </div>      
-                        
-          </div> */}
-          {/* <div className="p-0.5 sm:h-80 h-40 overflow-hidden relative flex items-center justify-center border border-gray-500 rounded-md">
-               <Image 
-                      src={m3}
-                      height={800}
-                      width={800}
-                      alt="Product Image"
-                      className='w-full  mx-auto rounded-t-md sm:rounded-md'
-                      />
-
-                <div className="absolute">
-                  <h2 className="text-gray-100 text-lg rtl:font-arabic border-2 border-gray-300 px-2 py-1 rounded bg-gray-800">اليمـن</h2>
-                  </div>      
-                        
-          </div> */}
       </div>
       </div>
        </div>
