1bf1eb23e4aeca0e8db682c3c7839e16fd664128
Alsumairi1990 <samnaddeen2000@gmail.com>
2024-11-04 07:57:07 -0500

    working on palns

--- app/[locale]/(pages-default)/plans/categories/[slug]/page.tsx
+++ app/[locale]/(pages-default)/plans/categories/[slug]/page.tsx
@@ -42,9 +42,25 @@ const category:PlanCategoryForFront = await categoryData.json();
                                />
             }
            </div>
+           <div className="py-8 flex w-11.4/12 mx-auto flex-col justify-center">
+            {locale === 'en' ? <div className="flex flex-col justify-center">
+              <h2 className="text-xl font-semibold ">
+                {category.title}
+              </h2>
+              <p className="text-md">{category.description}</p>
+            </div>:
+            <div className="flex font-arabic justify-center flex-col">
+              <h2 className="text-xl mb-1 font-semibold ">
+                {category.titleAr}
+              </h2>
+              <p className="text-md leading-7">{category.descriptionAr}</p>
+            </div>
+            }
+           </div>
             
-           <div className="w-11.6/12 my-10 mx-auto grid sm:grid-cols-75/25">
-              <div className="grid sm:grid-cols-3 gap-x-6">
+           <div className="w-11/12 my-10 mx-auto">
+         
+              <div className="grid sm:grid-cols-3 sm:gap-x-12 max-sm:gap-y-6  w-full">
                 {category && category.plans && 
                    category.plans.map((plan) => (
                     <PlanCard  plan = {plan} locale = {locale} messages = {messages}  />
