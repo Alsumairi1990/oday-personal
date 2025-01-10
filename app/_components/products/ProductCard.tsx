d7e0ea27c58879e26e229ad4fe69d5bae4d67c13
Alsumairi1990 <samnaddeen2000@gmail.com>
2025-01-10 18:31:49 +0800

    working on features

--- app/_components/products/ProductCard.tsx
+++ app/_components/products/ProductCard.tsx
@@ -17,13 +17,13 @@ const ProductCard = ({ product,locale,messages } : Props) => {
   const buy = (messages as any).Common.buy;
   return (
 
-    <div className=" py-3 text-center rounded-md shadow-xl border border-gray-200">
-     <div className="flex px-2  h-32 overflow-hidden justify-center">
-        {product.image && <img src={product.image} className='h-full w-full' alt={product.name} />}
+    <div className=" py-2 text-center rounded-md shadow-xl border border-gray-200">
+     <div className="flex px-2 h-56 sm:h-[17rem] overflow-hidden justify-center">
+        {product.image && <img src={product.image} className='h-full w-full rounded-t-md' alt={product.name} />}
      </div>
         {locale == 'en' ? <div className="mt-4 px-2 ">
             <p className="text-gray-800 text-xl mb-1 font-semibold dark:text-orange-500 ">{product.name}</p>
-            <p className="text-gray-500 text-sm tracking-8  leading-6 px-1.5 dark:text-gray-100 line-clamp-5 font-medium mb-4">{product.description}</p>
+            {/* <p className="text-gray-500 text-sm tracking-8  leading-6 px-1.5 dark:text-gray-100 line-clamp-5 font-medium mb-4">{product.description}</p> */}
             <div className="flex items-center bg-orange-500 px-2 py-1 mx-auto justify-center border  border-orange-500 w-fit rounded-md shadow-md">
               <a href="" className="text-gray-50   text-sm font-medium   ">{buy}</a>
             </div>
@@ -33,14 +33,14 @@ const ProductCard = ({ product,locale,messages } : Props) => {
         </div>
         : 
         <div className="mt-4 font-arabic px-2">
-            <p className="text-gray-800 text-lg font-bold dark:text-orange-500 mb-2">{product.nameAr}</p>
-            <p className="text-gray-500 text-sm tracking-8  leading-7 px-1.5 dark:text-gray-100 line-clamp-5 font-medium mb-4">{product.descriptionAr}</p>
+            <p className="text-gray-800 text-md font-bold dark:text-orange-500 mb-2">{product.nameAr}</p>
+            {/* <p className="text-gray-500 text-sm tracking-8  leading-7 px-1.5 dark:text-gray-100 line-clamp-3 font-medium mb-4">{product.descriptionAr}</p> */}
             <div className="flex px-3 gap-x-3 justify-center border-t border-t-gray-200 pt-3">
-             <div className="py-1 px-2 text-center bg-orange-600 border border-orange-600 rounded-md">
-              <Link href="/product" className="text-base rtl:font-arabic rtl:text-sm rtl:font-bold text-white">{buy}</Link>
+             <div className="py-0.5 px-2 text-center bg-orange-600 border border-orange-600 rounded-md">
+              <Link href="/product" className="text-sm rtl:font-arabic rtl:text-sm rtl:font-bold text-white">{buy}</Link>
              </div>
-             <div className="py-1 px-2 text-center border bg-gray-50 rounded-md">
-              <Link href="/product" className="text-base rtl:font-arabic rtl:font-semibold rtl:text-sm"> {details}</Link>
+             <div className="py-0.5 px-2 text-center border bg-gray-50 rounded-md">
+              <Link href="/product" className="text-sm rtl:font-arabic rtl:font-semibold rtl:text-sm"> {details}</Link>
              </div>
            </div>
         </div>
