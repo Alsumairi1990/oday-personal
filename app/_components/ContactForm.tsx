b1611e5e477af5e10a76e59eca698afb429ba582
Alsumairi1990 <samnaddeen2000@gmail.com>
2025-01-26 17:00:44 +0800

    new back  db changed

--- app/_components/ContactForm.tsx
+++ app/_components/ContactForm.tsx
@@ -7,8 +7,8 @@ import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 
 interface Props {
-    locale : string,
-    messages : AbstractIntlMessages
+    locale? : string,
+    messages? : AbstractIntlMessages
 }
 const ContactForm = ({locale,messages}:Props) => {
     const imagePath = 'https://d3mds3ychln71.cloudfront.net/img/title-bg3.png';
