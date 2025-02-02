e3331791defee942d57923d675ce91eeb48aa402
Alsumairi1990 <samnaddeen2000@gmail.com>
2025-02-03 06:20:02 +0800

    working on home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -162,20 +162,20 @@ const config = {
         arabic: ['Kufi Arabic', 'sans-serif'], // Custom name for Arabic font
       },
      
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: "0" },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: "0" },
+      //   },
+      // },
+      // animation: {
+      //   "accordion-down": "accordion-down 0.2s ease-out",
+      //   "accordion-up": "accordion-up 0.2s ease-out",
+      // },
     },
   },
   plugins: [require("tailwindcss-animate"), rtl],
